653a3c7479258087ac8f223b44583a5b6455f506
amelny <130101213+amelny@users.noreply.github.com>
2023-07-26 18:11:35 +0200

    Update microbit-central.py

--- microbit-central.py
+++ microbit-central.py
@@ -2,23 +2,22 @@
 from microbit import *
 import radio
 
-
 radio.config(group=23)
+radio.on()
 # Code in a 'while True:' loop repeats forever
 while True:
     display.show(Image.SMILE)
     sleep(400)
    
     while True:
-        radio.config(group=23)
-        radio.on()
+        
         message = radio.receive()
         if message:
             display.scroll(str(message))
             display.show(Image.YES)
-            radio.off()
+            
             sleep(400)
         else:
-            radio.off()
+            
             display.show(Image.ASLEEP)
             sleep(400)
